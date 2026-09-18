f0382463ec2e2e14a85c07ab612a82dd9c9ef239
Nutdanai Phansooksai <moozz@Moozz.local>
2026-09-18 13:20:35 +0200

    initial commit

---
+++ sw.js
@@ -0,0 +1,27 @@
+const C = 'wg-v3';
+const SHELL = ['./','./index.html','./style.css','./app.js','./data/items.json','./manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(C).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(k => Promise.all(k.filter(x => x !== C).map(x => caches.delete(x))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(r => {
+        const copy = r.clone();
+        caches.open(C).then(c => c.put(e.request, copy));
+        return r;
+      })
+      .catch(() => caches.match(e.request).then(m => m || caches.match('./index.html')))
+  );
+});
